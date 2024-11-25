92bad4dbcdcd35d03c941b8236fe32e02b3cd70f
tridibbanik17 <165870312+tridibbanik17@users.noreply.github.com>
2024-11-25 13:31:57 -0500

    Binary Search

---
+++ SearchInsert.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        // The index of the leftmost and rightmost element.
+        int left = 0, right = nums.length - 1;
+
+        // left is smaller than right always needs to be true in binary search
+        while (left <= right) {
+            // Take the average of the leftmost and the rightmost 
+            int mid = left + (right - left) / 2; // To prevent overflow
+            // If the element at the middle index is the target value, then the middle index is found.
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Move to the right half
+            } else {
+                right = mid - 1; // Move to the left half
+            }
+        }
+
+        // If the target element is not found, `left` points to the insertion index
+        return left;
+    }
+}
