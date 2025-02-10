0ec9c11825a6af40ea1a036b2307052cd65fc1b3
Tridib Banik <tridib.perfect@gmail.com>
2025-02-10 01:21:47 -0500

    Determine how many of the stones that exist are also jewels. (added comments)

--- JewelsAndStones.java
+++ JewelsAndStones.java
@@ -17,7 +17,9 @@
 import java.util.Scanner;
 class JewelsAndStones {
     public int numJewelsInStones(String jewels, String stones) {
+        // initialize counter
         int count = 0;
+        // For every character in jewels, check all characters in stones and increment counter every time a character in jewels is found in stones.
         for (int jewelCount = 0; jewelCount < jewels.length(); jewelCount++) {
             for (int stoneCount = 0; stoneCount < stones.length(); stoneCount++) {
                 if (jewels.charAt(jewelCount) == stones.charAt(stoneCount)) {
@@ -29,12 +31,14 @@ class JewelsAndStones {
     }
 
     public static void main(String[] args) {
+        // New object instance of JewelsAndStones class.
         JewelsAndStones obj = new JewelsAndStones();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the jewels String: ");
         String jewelsStr = scanner.nextLine();
         System.out.println("Enter the stones String: ");
         String stonesStr = scanner.nextLine();
+        // Print the output
         System.out.println("The number of stones that are also jewels is " + obj.numJewelsInStones(jewelsStr, stonesStr) + ".");
     }
 }
